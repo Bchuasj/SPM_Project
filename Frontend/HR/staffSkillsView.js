14e378805d8a87991306ed7be50e47d094b1ae0a
frostedtrees <t.r.t.trina@gmail.com>
2022-10-09 01:27:25 +0800

    update getAllSkills to show only non-deleted

--- Frontend/HR/staffSkillsView.js
+++ Frontend/HR/staffSkillsView.js
@@ -3,29 +3,32 @@ function getAllSkills() {
     axios.get("http://127.0.0.1:5006/skill")
         .then(function (response) {
             var skills = response.data.data.skills;
+            console.log(skills)
             for(let skill in skills){
-                skillsTable.innerHTML += `
-                <div class="row rounded border border-1 bg-white py-2 mb-2 d-flex align-items-center">
-                    <div class="col-sm-3 col-3">
-                        <b>${skills[skill].skillId}</b>
-                    </div>
-                    <div class="col-sm-6 col-6">
-                        <b>${skills[skill].skillName}</b>
-                    </div>
-                    <div class="col-sm col">
-                        <button type="button" class="btn btn-sm px-3 text-white" style="background-color: #106eea" data-bs-toggle="collapse" data-bs-target="#collapseExample${skills[skill].skillId}" aria-expanded="false" aria-controls="collapseExample${skills[skill].skillId}">View details</button>
-                    </div>
-                    <div class="collapse pt-2" id="collapseExample${skills[skill].skillId}">
-                        <div class="card card-body text-start">
-                            <p><b>Description</b></p>
-                            ${skills[skill].skillDesc}
+                // console.log(skills[skill])
+                if (skills[skill].isDeleted == false) {
+                    skillsTable.innerHTML += `
+                    <div class="row rounded border border-1 bg-white py-2 mb-2 d-flex align-items-center">
+                        <div class="col-sm-3 col-3">
+                            <b>${skills[skill].skillId}</b>
+                        </div>
+                        <div class="col-sm-6 col-6">
+                            <b>${skills[skill].skillName}</b>
+                        </div>
+                        <div class="col-sm col">
+                            <button type="button" class="btn btn-sm px-3 text-white" style="background-color: #106eea" data-bs-toggle="collapse" data-bs-target="#collapseExample${skills[skill].skillId}" aria-expanded="false" aria-controls="collapseExample${skills[skill].skillId}">View details</button>
+                        </div>
+                        <div class="collapse pt-2" id="collapseExample${skills[skill].skillId}">
+                            <div class="card card-body text-start">
+                                <p><b>Description</b></p>
+                                ${skills[skill].skillDesc}
+                            </div>
                         </div>
                     </div>
-                </div>
-                `
+                    `
                 }
             }
-        
-    )
+        }
+    )   
 }
 
