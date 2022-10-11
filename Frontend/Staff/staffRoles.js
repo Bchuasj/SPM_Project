199007092f9ac1057786bc5919da47b0f3389ac5
Pei Ming <76734355+seahpm99@users.noreply.github.com>
2022-10-11 15:09:42 +0800

    Fix staffRoles to show skills required by role

--- Frontend/Staff/staffRoles.js
+++ Frontend/Staff/staffRoles.js
@@ -6,7 +6,7 @@ function getAllRoles() {
             var counter = 0
             for(let role in roles){
                 table.innerHTML += `
-                <div class='row rounded border border-1 bg-white py-2 mb-2 d-flex align-items-center' data-bs-toggle='collapse' data-bs-target='#collapseExample${counter}' aria-expanded='false' aria-controls='collapseExample${counter}'>
+                <div id = ${roles[role.roleId]} class='row rounded border border-1 bg-white py-2 mb-2 d-flex align-items-center' data-bs-toggle='collapse' data-bs-target='#collapseExample${counter}' aria-expanded='false' aria-controls='collapseExample${counter}' onclick = "getRoleSkill(${roles[role].roleId})">
                 <div class='col-2'>
                   <b>${roles[role].roleId}</b>
                 </div>
@@ -18,13 +18,31 @@ function getAllRoles() {
                 </div>
                 <div class="collapse pt-2" id="collapseExample${counter}">
                     <div class="card card-body text-start">
-                    <p><b>Description</b></p>
-                    Learn how to pacify businessmen.
+                    <p ><b>Skills</b></p>
+                    <span id = "roleSkill${roles[role].roleId}"></span>
                     </div>
                 </div>`
                 counter++
             }
         })
+}
+
+// implment get role skills
+function getRoleSkill(roleId){
+  roleSkill = document.getElementById("roleSkill"+roleId);
+  axios.get("http://127.0.0.1:5006/role/"+roleId+"/skills")
+  .then(function (response) {
+    roleSkill.innerHTML = ""
+    var skills = response.data.data.skills;
+    if (skills){
+      for(let skill in skills){
+        roleSkill.innerHTML += `<span class="badge bg-primary" style = "margin-right:5px">${skills[skill].skillName}</span>`
+    }}
+  })
+  .catch(function (error) {
+    console.log(error)
+    roleSkill.innerHTML = `<span class="badge bg-danger">No skills</span>`
+  })
 }
 
 function getRole() {
@@ -41,7 +59,7 @@ function getRole() {
                 table.innerHTML = "";
                 table.innerHTML += `
                 <div class='row rounded border border-1 bg-white py-2 mb-2 d-flex align-items-center' data-bs-toggle='collapse' data-bs-target='#collapseExample${counter}' aria-expanded='false' aria-controls='collapseExample${counter}'>
-                <div class='col-2'>
+                <div class='col-2' id = "roleId">
                   <b>${roleId}</b>
                 </div>
                 <div class='col'>
