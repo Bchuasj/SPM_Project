c7bf46c5e407be0e628166f468200b0edb9eb206
Pei Ming <76734355+seahpm99@users.noreply.github.com>
2022-10-13 00:34:31 +0800

    create skill able to search by id and name now

--- Frontend/HR/hrCreateUpdate.js
+++ Frontend/HR/hrCreateUpdate.js
@@ -50,14 +50,16 @@ function getCourse() {
     tr = table.getElementsByTagName("tr");
     for (i = 0; i < tr.length; i++) {
       td = tr[i].getElementsByTagName("td")[1];
-      if (td) {
+      td1 = tr[i].getElementsByTagName("td")[0];
+      if (td || td1) {
         txtValue = td.textContent || td.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        txt2 = td1.textContent || td1.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1 || txt2.toUpperCase().indexOf(filter) > -1) {
           tr[i].style.display = "";
         } else {
           tr[i].style.display = "none";
         }
-      }       
+      }   
     }
   }
 
