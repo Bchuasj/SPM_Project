7d9685eadcc0e6242555c2cbb725f0cb1793daee
luqmanj <luqmanmn.2020@scis.smu.edu.sg>
2022-11-02 07:19:25 +0800

    implement search bar for search work Role

--- Frontend/Staff/staffCreateLearningJourney.js
+++ Frontend/Staff/staffCreateLearningJourney.js
@@ -534,8 +534,31 @@ function createLj(staffId){
 
         
 
+    }    
+
+}
+
+
+function getWorkRole() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("workRoleInput");
+    filter = input.value.toUpperCase();
+    filter = filter.replace(/ /g, '');
+    table = document.getElementById("viewWorkRoles");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[1];
+        td1 = tr[i].getElementsByTagName("td")[0];
+        if (td || td1) {
+            txtValue = td.textContent || td.innerText;
+            txtValue = txtValue.replace(/ /g, '')
+            txt2 = td1.textContent || td1.innerText;
+            txt2 = txt2.replace(/ /g, '')
+            if (txtValue.toUpperCase().indexOf(filter) > -1 || txt2.toUpperCase().indexOf(filter) > -1) {
+            tr[i].style.display = "";
+            } else {
+            tr[i].style.display = "none";
+            }
+        }   
     }
-
-    
-
 }
